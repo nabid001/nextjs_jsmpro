dfff6892bb032edffce653d05a8224ca236a7436
Nabid <nabid.explore@gmail.com>
2023-11-21 11:22:51 +0600

    search and filtering complete in header section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -31,25 +31,32 @@ const page = async ({ searchParams }: Props) => {
 
       <Filters />
 
-      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        <Header />
-        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          {resources.length > 0 ? (
-            resources.map((resource: any) => (
-              <ResourceCard
-                key={resource._id}
-                title={resource.title}
-                id={resource._id}
-                image={resource.image}
-                downloadNumber={resource.views}
-                downloadLink={resource.downloadLink}
-              />
-            ))
-          ) : (
-            <p className="body-regular text-white-400">No resources found</p>
-          )}
-        </div>
-      </section>
+      {(searchParams?.query || searchParams?.category) && (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            title="Resources"
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
+
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))
+            ) : (
+              <p className="body-regular text-white-400">No resources found</p>
+            )}
+          </div>
+        </section>
+      )}
     </main>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,36 @@
-const Header = () => {
-  return <div>Header</div>;
+interface Props {
+  type: string;
+  category: string;
+  query: string;
+}
+
+const Header = ({ type, category, query }: Props) => {
+  if (query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search result for "{query}" in{" "}
+        <span className="capitalize">{category}</span>
+      </h1>
+    );
+  }
+
+  if (query) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search result for "{query}"
+      </h1>
+    );
+  }
+
+  if (category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        <span className="capitalize">{category}</span>
+      </h1>
+    );
+  }
+
+  return <h1 className="heading3 self-start text-white-800">No Results</h1>;
 };
 
 export default Header;
